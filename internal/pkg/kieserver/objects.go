cf0f3e682bdcd65e445bc856fcbc755b90decaca
Babak Mozaffari <bmozaffa@redhat.com>
2018-09-12 23:55:26 -0700

    Corrected trial env problems that prevented proper connection between the pods

--- internal/pkg/kieserver/objects.go
+++ internal/pkg/kieserver/objects.go
@@ -113,6 +113,7 @@ func GetKieServer(cr *v1alpha1.App) []runtime.Object {
 	defaultEnv := defaults.ServerEnvironmentDefaults()
 	defaultEnv["KIE_SERVER_CONTROLLER_SERVICE"] = rhpamcentrServiceName
 	defaultEnv["RHPAMCENTR_MAVEN_REPO_SERVICE"] = rhpamcentrServiceName
+	defaultEnv["EXECUTION_SERVER_ROUTE_NAME"] = serviceName
 	dc.Spec.Template.Spec.Containers[0].Env = shared.GetEnvVars(defaultEnv, cr.Spec.Server.Env)
 
 	service := &corev1.Service{
@@ -140,7 +141,7 @@ func GetKieServer(cr *v1alpha1.App) []runtime.Object {
 					TargetPort: intstr.FromInt(8080),
 				},
 			},
-			Selector: labels,
+			Selector: map[string]string{"deploymentconfig": serviceName,},
 		},
 	}
 
